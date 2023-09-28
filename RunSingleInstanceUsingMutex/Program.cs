596dd7859e7c1a20490c1bc90cfae0c52f223a97
DESKTOP-83N2NSV\Admino <camtv.frmobile@gmail.com>
2023-09-28 11:32:10 +0700

    add

---
+++ RunSingleInstanceUsingMutex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunSingleInstanceUsingMutex
+{
+    static class Program
+    {
+        static Mutex appMutex;
+        static string appGuid;
+
+        [STAThread]
+        static void Main()
+        {
+            bool createdNew;
+            appGuid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
+            appMutex = new Mutex(true, appGuid, out createdNew);
+            if (!createdNew)
+            {
+                Environment.Exit(0);
+                return;
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
